9f6210563048fa189e99ac8c0892af14d7a4d4a2
Brady Buchholz <bradybuchholz@gmail.com>
2022-10-04 13:46:05 -0400

    buttons for exercises route and workouts route created and functional

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import ExerciseModal from '../Modals/ExerciseModal.js';
 import WorkoutModal from '../Modals/WorkoutModal.js';
+import Button from '@mui/material/Button';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
 
 function NavBar() {
   return (
     <React.Fragment>
       <div>
         <nav className="nav-container">
+          <Link to="/exercises">
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              endIcon={<FitnessCenterIcon style={{ color: 'black' }} />}
+            >
+              Exercises
+            </Button>
+          </Link>
+          <Link to="/workouts">
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              endIcon={<ImportContactsIcon style={{ color: 'black' }} />}
+            >
+              Workouts
+            </Button>
+          </Link>
           <ExerciseModal />
           <WorkoutModal />
         </nav>
