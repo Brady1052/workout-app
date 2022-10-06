22042966c1ba417eae9d1212d08982792a2fd3ad
Brady Buchholz <bradybuchholz@gmail.com>
2022-10-06 16:24:42 -0400

    using material ui grid and navbar to replace bootstrap

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/Layout/Navbar';
-import ExerciseCard from './components/UI/ExerciseCard';
-import WorkoutTable from './components/UI/WorkoutTable';
 import Exercises from './pages/Exercises';
 import Workouts from './pages/Workouts';
-
+import 'fontsource-roboto';
+import Typography from '@mui/material/Typography';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import createMuiTheme from '@mui/material/styles/createTheme';
 function App() {
+  const theme = createMuiTheme({
+    typography: {
+      h1: { fontSize: '50rem' },
+    },
+  });
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <NavBar />
-      {/* <ExerciseCard />
-      <WorkoutTable /> */}
+      <Typography style={{ marginTop: '8rem' }} variant="h2">
+        Welcome to Workout
+      </Typography>
       <Routes>
         <Route path="/exercises" element={<Exercises />} />
         <Route path="/workouts" element={<Workouts />} />
       </Routes>
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
 
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -6,38 +6,98 @@ import WorkoutModal from '../Modals/WorkoutModal.js';
 import Button from '@mui/material/Button';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
-import { ButtonGroup } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { AppBar } from '@mui/material';
+import { Toolbar } from '@mui/material';
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Modal } from '@mui/material';
+import { Box } from '@mui/material';
+import { Typography } from '@mui/material';
 
 function NavBar() {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <React.Fragment>
-      <div>
-        <nav className="nav-container">
-          <Link to="/exercises">
-            <Button
-              variant="contained"
-              color="primary"
-              size="large"
-              endIcon={<FitnessCenterIcon style={{ color: 'black' }} />}
-            >
-              Exercises
-            </Button>
-          </Link>
-
-          <Link to="/workouts">
-            <Button
-              variant="contained"
-              color="primary"
-              size="large"
-              endIcon={<ImportContactsIcon style={{ color: 'black' }} />}
-            >
-              Workouts
-            </Button>
-          </Link>
-          <ExerciseModal />
-          <WorkoutModal />
-        </nav>
-      </div>
+      <AppBar>
+        <Toolbar>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          {/* <div className="nav-container"> */}
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button variant="contained" onClick={handleOpen}>
+                Open
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <Typography
+                      id="modal-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
+                      Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                      Duis mollis, est non commodo luctus, nisi erat porttitor
+                      ligula.
+                    </Typography>
+                  </Box>
+                </Modal>
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Link to="/exercises">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  endIcon={<FitnessCenterIcon style={{ color: 'black' }} />}
+                >
+                  Exercises
+                </Button>
+              </Link>
+            </Grid>
+            <Grid item xs={3}>
+              <Link to="/workouts">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  endIcon={<ImportContactsIcon style={{ color: 'black' }} />}
+                >
+                  Workouts
+                </Button>
+              </Link>
+            </Grid>
+            <Grid item xs={3}>
+              <ExerciseModal />
+            </Grid>
+            <Grid item>
+              <WorkoutModal />
+            </Grid>
+          </Grid>
+          {/* </div> */}
+        </Toolbar>
+      </AppBar>
     </React.Fragment>
   );
 }
